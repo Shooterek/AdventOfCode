9818d2595ba4334a6123169a6d5285263d3ad555
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-08 22:03:03 +0100

    Add second way of solving task 2

--- AoE2021/Day8cs.cs
+++ AoE2021/Day8cs.cs
@@ -28,6 +28,49 @@ namespace AoE2021
             var lines = this._inputLoader.LoadStringListInput();
             var finalSum = lines.Aggregate(0, (value, line) =>
             {
+                var input = line.Split("|")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var result = line.Split("|")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var dict = new List<(string, int)>();
+
+                var digit1 = input.Single(x => x.Length == 2);
+                var digit4 = input.Single(x => x.Length == 4);
+                var diff1and4 = digit4.Except(digit1);
+                var digit7 = input.Single(x => x.Length == 3);
+                var diff1and7 = digit7.Except(digit1);
+                var digit8 = input.Single(x => x.Length == 7);
+                var digit6 = input.Single(x => x.Length == 6 && x.Intersect(digit1).Count() == 1);
+                var digit0 = input.Single(x => x.Length == 6 && x.Intersect(diff1and4).Count() == 1);
+                var digit9 = input.Single(x => x.Length == 6 && x != digit0 && x != digit6);
+                var digit5 = input.Single(x => x.Length == 5 && x.Intersect(digit1).Count() == 1 && digit9.Intersect(x).Count() == 5);
+                var digit2 = input.Single(x => x.Length == 5 && x.Intersect(digit5).Count() == 3);
+                var intsersect2and5 = digit2.Intersect(digit5).Append(digit1[1]).Append(digit1[0]);
+                var digit3 = input.Single(x => x.Length == 5 && x.Intersect(intsersect2and5).Count() == 5);
+
+                dict.Add((digit0, 0));
+                dict.Add((digit1, 1));
+                dict.Add((digit2, 2));
+                dict.Add((digit3, 3));
+                dict.Add((digit4, 4));
+                dict.Add((digit5, 5));
+                dict.Add((digit6, 6));
+                dict.Add((digit7, 7));
+                dict.Add((digit8, 8));
+                dict.Add((digit9, 9));
+
+                return value + dict.First(x => x.Item1.Length == result[0].Length && x.Item1.Intersect(result[0]).Count() == x.Item1.Length).Item2 * 1000
+                       + dict.First(x => x.Item1.Length == result[1].Length && x.Item1.Intersect(result[1]).Count() == x.Item1.Length).Item2 * 100
+                       + dict.First(x => x.Item1.Length == result[2].Length && x.Item1.Intersect(result[2]).Count() == x.Item1.Length).Item2 * 10
+                       + dict.First(x => x.Item1.Length == result[3].Length && x.Item1.Intersect(result[3]).Count() == x.Item1.Length).Item2;
+            });
+            return finalSum;
+        }
+
+        private object SecondTaskOnBits()
+        {
+            var lines = this._inputLoader.LoadStringListInput();
+            var finalSum = lines.Aggregate(0, (value, line) =>
+            {
                 var input = line.Split("|")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => (number: MapToInt(x), length: x.Length));
                 var result = line.Split("|")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(MapToInt).ToArray();
 
