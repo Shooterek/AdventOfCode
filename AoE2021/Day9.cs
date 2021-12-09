5805a6addecf0bf0622f3642e836225ba202e545
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-09 21:05:11 +0100

    Day 9 task 2

--- AoE2021/Day9.cs
+++ AoE2021/Day9.cs
@@ -44,17 +44,67 @@ namespace AoE2021
         {
             var lines = this._inputLoader.LoadStringListInput().Select(x => x.ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray()).ToArray();
             var sum = 0;
+            var yLength = lines.Length;
+            var xLength = lines[0].Length;
+            var stack = new Stack<(int x, int y)>();
+            var sizes = new List<int>();
+
             for (int i = 0; i < lines.Length; i++)
             {
                 for (int j = 0; j < lines[i].Length; j++)
                 {
                     if (lines[i][j] != 9)
-                        sum++;
+                    {
+                        sizes.Add(BackTrack(j, i));
+                    }
                 }
             }
 
+            return sizes.OrderByDescending(x => x).Take(3).Aggregate(1, (aggr, val) => aggr * val);
 
-            return sum;
+            int BackTrack(int x, int y)
+            {
+                var size = 1;
+                var result = new List<int>();
+                stack.Push((x, y));
+                while (stack.Count >= 0)
+                {
+                    result.Add(lines[y][x]);
+                    lines[y][x] = 9;
+                    if (y + 1 < yLength && lines[y + 1][x] != 9)
+                    {
+                        stack.Push((x, y));
+                        y += 1;
+                        size++;
+                    }
+                    else if (y - 1 >= 0 && lines[y - 1][x] != 9)
+                    {
+                        stack.Push((x, y));
+                        y -= 1;
+                        size++;
+                    }
+                    else if (x + 1 < xLength && lines[y][x + 1] != 9)
+                    {
+                        stack.Push((x, y));
+                        x += 1;
+                        size++;
+                    }
+                    else if (x - 1 >= 0 && lines[y][x - 1] != 9)
+                    {
+                        stack.Push((x, y));
+                        x -= 1;
+                        size++;
+                    }
+                    else
+                    {
+                        if (stack.Count > 0)
+                            (x, y) = stack.Pop();
+                        else return size;
+                    }
+                }
+
+                return size;
+            }
         }
     }        
 }
