c6b7086941acd3200dfda76265b541f5bf583c72
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-11 12:28:59 +0100

    Refactor day 11

--- AoE2021/Day11.cs
+++ AoE2021/Day11.cs
@@ -13,217 +13,46 @@ namespace AoE2021
 
         protected override object FirstTask()
 		{
-			var grid = this._inputLoader.LoadStringListInput()
-				.Select(x => x.ToCharArray().Select(x => (val: (int)char.GetNumericValue(x), flashed: false)).ToArray()).ToArray();
-
+			var input = this._inputLoader.LoadStringListInput()
+				.Select(x => x.ToCharArray().Select(x => (int)char.GetNumericValue(x)).ToArray()).ToArray();
+			var grid = new OctopusGrid(input);
 			var flashedCount = 0;
 
 			for (int i = 0; i < 100; i++)
 			{
-				Console.WriteLine(i);
-				IncreaseAllByOne();
+				grid.IncreaseAllByOne();
 
-				while (ShouldAnyOctopusFlash())
-				{
-					FlashAll();
-				}
+				while (grid.ShouldAnyOctopusFlash())
+					flashedCount += grid.FlashAll();
 
-				ZeroAllFlashed();
-				PrintGrid();
+				grid.ZeroAllFlashed();
 			}
 
 			return flashedCount;
-
-			void IncreaseAllByOne()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						grid[i][j].val += 1;
-					}
-				}
-			}
-
-			bool ShouldAnyOctopusFlash()
-			{
-				return grid.Any(x => x.Any(x => x.val > 9 && !x.flashed));
-			}
-
-			void FlashAll()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						if (grid[i][j].val > 9 && !grid[i][j].flashed)
-						{
-							grid[i][j].flashed = true;
-							IncreaseAllNeighbours(i, j);
-						}
-					}
-				}
-			}
-
-			void IncreaseAllNeighbours(int y, int x)
-			{
-				var gridLength = grid.Length;
-				grid[y][x].val += 1;
-				if (y < gridLength - 1)
-				{
-					grid[y + 1][x].val += 1;
-					if (x < grid[y].Length - 1)
-						grid[y + 1][x + 1].val += 1;
-					if (x > 0)
-						grid[y + 1][x - 1].val += 1;
-				}
-				if (y > 0)
-				{
-					grid[y - 1][x].val += 1;
-					if (x < grid[y].Length - 1)
-						grid[y - 1][x + 1].val += 1;
-					if (x > 0)
-						grid[y - 1][x - 1].val += 1;
-				}
-				if (x > 0)
-				{
-					grid[y][x - 1].val += 1;
-				}
-				if (x < gridLength - 1)
-				{
-					grid[y][x + 1].val += 1;
-				}
-			}
-
-			void ZeroAllFlashed()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						if (grid[i][j].flashed)
-						{
-							grid[i][j].val = 0;
-							grid[i][j].flashed = false;
-							flashedCount++;
-						}
-					}
-				}
-			}
-
-			void PrintGrid()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						Console.Write(grid[i][j].val + ", ");
-					}
-					Console.WriteLine();
-				}
-			}
         }
 
 		protected override object SecondTask()
 		{
-			var grid = this._inputLoader.LoadStringListInput()
-				.Select(x => x.ToCharArray().Select(x => (val: (int)char.GetNumericValue(x), flashed: false)).ToArray()).ToArray();
+			var input = this._inputLoader.LoadStringListInput()
+				.Select(x => x.ToCharArray().Select(x => (int)char.GetNumericValue(x)).ToArray()).ToArray();
+			var grid = new OctopusGrid(input);
 			var iteration = 1;
 
 			while (true)
 			{
-				IncreaseAllByOne();
-				Console.WriteLine(iteration);
+				grid.IncreaseAllByOne();
 
-				while (ShouldAnyOctopusFlash())
-				{
-					FlashAll();
-				}
+				while (grid.ShouldAnyOctopusFlash())
+					grid.FlashAll();
 
-				ZeroAllFlashed();
+				grid.ZeroAllFlashed();
 
-				if (grid.All(x => x.All(x => x.val == 0)))
+				if (grid.DidAllFlashTogether())
 					break;
 				iteration++;
 			}
 
 			return iteration;
-
-			void IncreaseAllByOne()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						grid[i][j].val += 1;
-					}
-				}
-			}
-
-			bool ShouldAnyOctopusFlash()
-			{
-				return grid.Any(x => x.Any(x => x.val > 9 && !x.flashed));
-			}
-
-			void FlashAll()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						if (grid[i][j].val > 9 && !grid[i][j].flashed)
-						{
-							grid[i][j].flashed = true;
-							IncreaseAllNeighbours(i, j);
-						}
-					}
-				}
-			}
-
-			void IncreaseAllNeighbours(int y, int x)
-			{
-				var gridLength = grid.Length;
-				grid[y][x].val += 1;
-				if (y < gridLength - 1)
-				{
-					grid[y + 1][x].val += 1;
-					if (x < grid[y].Length - 1)
-						grid[y + 1][x + 1].val += 1;
-					if (x > 0)
-						grid[y + 1][x - 1].val += 1;
-				}
-				if (y > 0)
-				{
-					grid[y - 1][x].val += 1;
-					if (x < grid[y].Length - 1)
-						grid[y - 1][x + 1].val += 1;
-					if (x > 0)
-						grid[y - 1][x - 1].val += 1;
-				}
-				if (x > 0)
-				{
-					grid[y][x - 1].val += 1;
-				}
-				if (x < gridLength - 1)
-				{
-					grid[y][x + 1].val += 1;
-				}
-			}
-
-			void ZeroAllFlashed()
-			{
-				for (int i = 0; i < grid.Length; i++)
-				{
-					for (int j = 0; j < grid[i].Length; j++)
-					{
-						if (grid[i][j].flashed)
-						{
-							grid[i][j].val = 0;
-							grid[i][j].flashed = false;
-						}
-					}
-				}
-			}
 		}
     }        
 }
--- AoE2021/Utils/OctopusGrid.cs
+++ AoE2021/Utils/OctopusGrid.cs
@@ -1,13 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace AoE2021.Utils
 {
 	public class OctopusGrid
 	{
+		public OctopusGrid(int[][] grid)
+		{
+			this.Grid = grid.Select(x => x.Select(y => (val: y, flashed: false)).ToArray()).ToArray();
+		}
+
 		public (int val, bool flashed)[][] Grid { get; }
+
+		public void IncreaseAllByOne()
+		{
+			for (int i = 0; i < this.Grid.Length; i++)
+			{
+				for (int j = 0; j < this.Grid[i].Length; j++)
+				{
+					this.Grid[i][j].val += 1;
+				}
+			}
+		}
+
+		public bool ShouldAnyOctopusFlash()
+		{
+			return this.Grid.Any(x => x.Any(x => x.val > 9 && !x.flashed));
+		}
+
+		public int FlashAll()
+		{
+			int count = 0;
+			for (int i = 0; i < this.Grid.Length; i++)
+			{
+				for (int j = 0; j < this.Grid[i].Length; j++)
+				{
+					if (this.Grid[i][j].val > 9 && !this.Grid[i][j].flashed)
+					{
+						this.Grid[i][j].flashed = true;
+						IncreaseAllNeighbours(i, j);
+						count++;
+					}
+				}
+			}
+
+			return count;
+		}
+
+		public void IncreaseAllNeighbours(int y, int x)
+		{
+			var gridLength = this.Grid.Length;
+			this.Grid[y][x].val += 1;
+			if (y < gridLength - 1)
+			{
+				this.Grid[y + 1][x].val += 1;
+				if (x < this.Grid[y].Length - 1)
+					this.Grid[y + 1][x + 1].val += 1;
+				if (x > 0)
+					this.Grid[y + 1][x - 1].val += 1;
+			}
+			if (y > 0)
+			{
+				this.Grid[y - 1][x].val += 1;
+				if (x < this.Grid[y].Length - 1)
+					this.Grid[y - 1][x + 1].val += 1;
+				if (x > 0)
+					this.Grid[y - 1][x - 1].val += 1;
+			}
+			if (x > 0)
+			{
+				this.Grid[y][x - 1].val += 1;
+			}
+			if (x < gridLength - 1)
+			{
+				this.Grid[y][x + 1].val += 1;
+			}
+		}
+
+		public void ZeroAllFlashed()
+		{
+			for (int i = 0; i < this.Grid.Length; i++)
+			{
+				for (int j = 0; j < this.Grid[i].Length; j++)
+				{
+					if (this.Grid[i][j].flashed)
+					{
+						this.Grid[i][j].val = 0;
+						this.Grid[i][j].flashed = false;
+					}
+				}
+			}
+		}
+
+		public bool DidAllFlashTogether()
+			=> this.Grid.All(x => x.All(x => x.val == 0));
 	}
 }
