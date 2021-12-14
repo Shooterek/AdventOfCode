236980e726752efe98da0f21957b9d01099c432d
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-14 20:45:13 +0100

    day 14 t1 on list

--- AoE2021/Day14.cs
+++ AoE2021/Day14.cs
@@ -15,25 +15,22 @@ namespace AoE2021
         protected override object FirstTask()
 		{
 			var input = this._inputLoader.LoadStringBatches();
-			var instr = input[0];
+			var instr = input[0].ToList();
 
 			var instructions = input[1].Split("\r").Select(x =>
 			{
 				var split = x.Split(" -> ");
-				return (instr: split[0].Trim(), result: split[1]);
-			}).ToList();
+				return (instr: split[0].Trim(), result: split[1].First());
+			}).ToDictionary(x => x.instr, x => x.result);
 
 			for (int i = 0; i < 10; i++)
 			{
-				var sb = new StringBuilder();
-				sb.Append(instr);
-				for (int j = 0; j < instr.Length - 1; j++)
+				for (int j = 0; j < instr.Count - 1; j += 2)
 				{
 					var z = $"{instr[j]}{instr[j + 1]}";
-					var charToInsert = instructions.First(x => x.instr == z).result;
-					sb.Insert(1 + j * 2, charToInsert);
+					var charToInsert = instructions[z];
+					instr.Insert(1 + j, charToInsert);
 				}
-				instr = sb.ToString();
 			}
 
 			var x = instr.GroupBy(x => x).Select(x => x.Count()).OrderByDescending(y => y);
@@ -49,7 +46,7 @@ namespace AoE2021
 			var instructions = input[1].Split("\r").Select(x =>
 			{
 				var split = x.Split(" -> ");
-				return (instr: split[0].Trim(), result: split[1]);
+				return (instr: split[0].Trim(), result: split[1].First());
 			}).ToDictionary(x => x.instr, x => x.result);
 
 			for (int i = 0; i < 10; i++)
@@ -59,8 +56,7 @@ namespace AoE2021
 				for (int j = 0; j < instr.Length - 1; j++)
 				{
 					var z = $"{instr[j]}{instr[j + 1]}";
-					var charToInsert = instructions[z];
-					sb.Insert(1 + j * 2, charToInsert);
+					sb.Insert(1 + j * 2, instructions[z]);
 				}
 				instr = sb.ToString();
 			}
