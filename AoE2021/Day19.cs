aad95714a205c0ee500af5775e88888599f5b339
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-19 21:04:47 +0100

    Day 19 both tasks

--- AoE2021/Day19.cs
+++ AoE2021/Day19.cs
@@ -18,7 +18,9 @@ namespace AoE2021
 		{
 			var input = this._inputLoader.LoadStringBatches();
 			var scans = input.Select(x => x.Split('\r').ToList());
-			var scansDictionary = new Dictionary<string, List<Point>>();
+			var coordinatesPerScan = new Dictionary<string, List<Coordinates>>();
+			var scanners = new List<Coordinates>();
+			scanners.Add(new(0, 0, 0));
 
 			foreach (var scan in scans)
 			{
@@ -32,188 +34,417 @@ namespace AoE2021
 					list.Add(new(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])));
 				}
 
-				foreach (var point in list)
-				{
-					var distances = new List<Distance>();
-					foreach (var point2 in list.Where(x => x != point))
-					{
-						distances.Add(point.GetDistance(point2));
-					}
-
-					listOfPoints.Add(new(point, distances));
-				}
-
-				scansDictionary.Add(scan[0], listOfPoints);
+				coordinatesPerScan.Add(scan[0], list);
 			}
-
-			var s0 = scansDictionary.First(x => x.Key.Contains('0'));
-			var s1 = scansDictionary.First(x => x.Key.Contains('1'));
-
-			Point? mainScanP1 = null, newScanP1 = null, mainScanP2 = null, newScanP2 = null;
-			foreach (var p1 in s1.Value)
+			while (coordinatesPerScan.Count > 1)
 			{
-				foreach (var p0 in s0.Value)
+
+				var pointsPerScan = new Dictionary<string, List<Point>>();
+				foreach (var scan in coordinatesPerScan)
 				{
-					var ins = p0.DistancesToOtherPoints.Intersect(p1.DistancesToOtherPoints, new DistanceEqualityComparer()).ToList();
-					if (ins.Count() >= 11)
+					var points = new List<Point>();
+					foreach (var point in scan.Value)
 					{
-						if (mainScanP1 == null)
+						var distances = new List<Distance>();
+						foreach (var point2 in scan.Value.Where(x => x != point))
 						{
-							mainScanP1 = p0;
-							newScanP1 = p1;
+							distances.Add(point.GetDistance(point2));
 						}
-						else if (mainScanP2 == null)
+						points.Add(new(point, distances));
+					}
+					pointsPerScan[scan.Key] = points;
+				}
+
+				var s0 = pointsPerScan.First(x => x.Key.Contains('0'));
+
+				foreach (var s1 in pointsPerScan.Skip(1))
+				{
+					Point? mainScanP1 = null, newScanP1 = null, mainScanP2 = null, newScanP2 = null;
+					foreach (var p1 in s1.Value)
+					{
+						foreach (var p0 in s0.Value)
 						{
-							mainScanP2 = p0;
-							newScanP2 = p1;
+							var ins = p0.DistancesToOtherPoints.Intersect(p1.DistancesToOtherPoints, new DistanceEqualityComparer()).ToList();
+							if (ins.Count() >= 11)
+							{
+								if (mainScanP1 == null)
+								{
+									mainScanP1 = p0;
+									newScanP1 = p1;
+								}
+								else if (mainScanP2 == null)
+								{
+									mainScanP2 = p0;
+									newScanP2 = p1;
+								}
+							}
 						}
-						Console.WriteLine($"scan 0: {p0.Coordinates}, scan 1: {p1.Coordinates}");
+					}
+
+					int x1Dif, y1Dif, z1Dif, x2Dif, y2Dif, z2Dif;
+					if (mainScanP1 != null)
+					{
+						x1Dif = mainScanP1.Coordinates.X - mainScanP2.Coordinates.X;
+						x2Dif = newScanP1.Coordinates.X - newScanP2.Coordinates.X;
+
+						y1Dif = mainScanP1.Coordinates.Y - mainScanP2.Coordinates.Y;
+						y2Dif = newScanP1.Coordinates.Y - newScanP2.Coordinates.Y;
+
+						z1Dif = mainScanP1.Coordinates.Z - mainScanP2.Coordinates.Z;
+						z2Dif = newScanP1.Coordinates.Z - newScanP2.Coordinates.Z;
+
+						var map = new Dictionary<string, string>();
+						string xMap = null, yMap = null, zMap = null;
+						#region Create map
+						if (Math.Abs(x1Dif) == Math.Abs(x2Dif))
+						{
+							if (x1Dif == x2Dif)
+							{
+								xMap = "x";
+							}
+							else
+							{
+								xMap = "-x";
+							}
+						}
+						else if (Math.Abs(x1Dif) == Math.Abs(y2Dif))
+						{
+							if (x1Dif == y2Dif)
+							{
+								xMap = "y";
+							}
+							else
+							{
+								xMap = "-y";
+							}
+						}
+						else if (Math.Abs(x1Dif) == Math.Abs(z2Dif))
+						{
+							if (x1Dif == z2Dif)
+							{
+								xMap = "z";
+							}
+							else
+							{
+								xMap = "-z";
+							}
+						}
+
+						if (Math.Abs(y1Dif) == Math.Abs(x2Dif))
+						{
+							if (y1Dif == x2Dif)
+							{
+								yMap = "x";
+							}
+							else
+							{
+								yMap = "-x";
+							}
+						}
+						else if (Math.Abs(y1Dif) == Math.Abs(y2Dif))
+						{
+							if (y1Dif == y2Dif)
+							{
+								yMap = "y";
+							}
+							else
+							{
+								yMap = "-y";
+							}
+						}
+						else if (Math.Abs(y1Dif) == Math.Abs(z2Dif))
+						{
+							if (y1Dif == z2Dif)
+							{
+								yMap = "z";
+							}
+							else
+							{
+								yMap = "-z";
+							}
+						}
+
+						if (Math.Abs(z1Dif) == Math.Abs(x2Dif))
+						{
+							if (z1Dif == x2Dif)
+							{
+								zMap = "x";
+							}
+							else
+							{
+								zMap = "-x";
+							}
+						}
+						else if (Math.Abs(z1Dif) == Math.Abs(y2Dif))
+						{
+							if (z1Dif == y2Dif)
+							{
+								zMap = "y";
+							}
+							else
+							{
+								zMap = "-y";
+							}
+						}
+						else if (Math.Abs(z1Dif) == Math.Abs(z2Dif))
+						{
+							if (z1Dif == z2Dif)
+							{
+								zMap = "z";
+							}
+							else
+							{
+								zMap = "-z";
+							}
+						}
+						#endregion
+
+						var newPointCoords = new Coordinates(newScanP1.Coordinates.GetByAxis(xMap), newScanP1.Coordinates.GetByAxis(yMap), newScanP1.Coordinates.GetByAxis(zMap));
+						var mainPointCoords = mainScanP1.Coordinates;
+						var xDiff = newPointCoords.X - mainPointCoords.X;
+						var yDiff = newPointCoords.Y - mainPointCoords.Y;
+						var zDiff = newPointCoords.Z - mainPointCoords.Z;
+
+						scanners.Add(new(xDiff, yDiff, zDiff));
+						var mainScanPoints = s0.Value.Select(x => x.Coordinates);
+						var pointsToAdd = new List<Coordinates>();
+
+						foreach (var point in s1.Value.Select(x => x.Coordinates))
+						{
+							var z = new Coordinates(point.GetByAxis(xMap) - xDiff, point.GetByAxis(yMap) - yDiff, point.GetByAxis(zMap) - zDiff);
+							pointsToAdd.Add(z);
+						}
+
+						var all = mainScanPoints.Union(pointsToAdd);
+						coordinatesPerScan[s0.Key] = all.ToList();
+						coordinatesPerScan.Remove(s1.Key);
+						break;
 					}
 				}
 			}
 
-			int x1Dif, y1Dif, z1Dif, x2Dif, y2Dif, z2Dif;
-			if (mainScanP1 != null)
-			{
-				x1Dif = mainScanP1.Coordinates.X - mainScanP2.Coordinates.X;
-				x2Dif = newScanP1.Coordinates.X - newScanP2.Coordinates.X;
-
-				y1Dif = mainScanP1.Coordinates.Y - mainScanP2.Coordinates.Y;
-				y2Dif = newScanP1.Coordinates.Y- newScanP2.Coordinates.Y;
-
-				z1Dif = mainScanP1.Coordinates.Z - mainScanP2.Coordinates.Z;
-				z2Dif = newScanP1.Coordinates.Z - newScanP2.Coordinates.Z;
-
-				var map = new Dictionary<string, string>();
-				string xMap = null, yMap = null, zMap = null;
-				#region Create map
-				if (Math.Abs(x1Dif) == Math.Abs(x2Dif))
-				{
-					if (x1Dif == x2Dif)
-					{
-						xMap = "x";
-					}
-					else
-					{
-						xMap = "-x";
-					}
-				}
-				else if (Math.Abs(x1Dif) == Math.Abs(y2Dif))
-				{
-					if (x1Dif == y2Dif)
-					{
-						xMap = "y";
-					}
-					else
-					{
-						xMap = "-y";
-					}
-				}
-				else if (Math.Abs(x1Dif) == Math.Abs(z2Dif))
-				{
-					if (x1Dif == z2Dif)
-					{
-						xMap = "z";
-					}
-					else
-					{
-						xMap = "-z";
-					}
-				}
-
-				if (Math.Abs(y1Dif) == Math.Abs(x2Dif))
-				{
-					if (y1Dif == x2Dif)
-					{
-						yMap = "x";
-					}
-					else
-					{
-						yMap = "-x";
-					}
-				}
-				else if (Math.Abs(y1Dif) == Math.Abs(y2Dif))
-				{
-					if (y1Dif == y2Dif)
-					{
-						yMap = "y";
-					}
-					else
-					{
-						yMap = "-y";
-					}
-				}
-				else if (Math.Abs(y1Dif) == Math.Abs(z2Dif))
-				{
-					if (y1Dif == z2Dif)
-					{
-						yMap = "z";
-					}
-					else
-					{
-						yMap = "-z";
-					}
-				}
-
-				if (Math.Abs(z1Dif) == Math.Abs(x2Dif))
-				{
-					if (z1Dif == x2Dif)
-					{
-						zMap = "x";
-					}
-					else
-					{
-						zMap = "-x";
-					}
-				}
-				else if (Math.Abs(z1Dif) == Math.Abs(y2Dif))
-				{
-					if (z1Dif == y2Dif)
-					{
-						zMap = "y";
-					}
-					else
-					{
-						zMap = "-y";
-					}
-				}
-				else if (Math.Abs(z1Dif) == Math.Abs(z2Dif))
-				{
-					if (z1Dif == z2Dif)
-					{
-						zMap = "z";
-					}
-					else
-					{
-						zMap = "-z";
-					}
-				}
-				#endregion
-
-				var newPointCoords = new Coordinates(newScanP1.Coordinates.GetByAxis(xMap), newScanP1.Coordinates.GetByAxis(yMap), newScanP1.Coordinates.GetByAxis(zMap));
-				var mainPointCoords = mainScanP1.Coordinates;
-				var xDiff = newPointCoords.X - mainPointCoords.X;
-				var yDiff = newPointCoords.Y - mainPointCoords.Y;
-				var zDiff = newPointCoords.Z - mainPointCoords.Z;
-				var mainScanPoints = s0.Value.Select(x => x.Coordinates);
-				var pointsToAdd = new List<Coordinates>();
-
-				foreach (var point in s1.Value.Select(x => x.Coordinates))
-				{
-					var z = new Coordinates(point.GetByAxis(xMap) - xDiff, point.GetByAxis(yMap) - yDiff, point.GetByAxis(zMap) - zDiff);
-					pointsToAdd.Add(z);
-				}
-
-				var all = mainScanPoints.Union(pointsToAdd);
-			}
-
-
-			return "";
+			return coordinatesPerScan.First().Value.Count();
 		}
 
 		protected override object SecondTask()
 		{
-			return "";
+			var input = this._inputLoader.LoadStringBatches();
+			var scans = input.Select(x => x.Split('\r').ToList());
+			var coordinatesPerScan = new Dictionary<string, List<Coordinates>>();
+			var scanners = new List<Coordinates>();
+
+			foreach (var scan in scans)
+			{
+				var points = scan.Skip(1);
+				var list = new List<Coordinates>();
+				var listOfPoints = new List<Point>();
+
+				foreach (var point in points)
+				{
+					var split = point.Split(',');
+					list.Add(new(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])));
+				}
+
+				coordinatesPerScan.Add(scan[0], list);
+			}
+			while (coordinatesPerScan.Count > 1)
+			{
+
+				var pointsPerScan = new Dictionary<string, List<Point>>();
+				foreach (var scan in coordinatesPerScan)
+				{
+					var points = new List<Point>();
+					foreach (var point in scan.Value)
+					{
+						var distances = new List<Distance>();
+						foreach (var point2 in scan.Value.Where(x => x != point))
+						{
+							distances.Add(point.GetDistance(point2));
+						}
+						points.Add(new(point, distances));
+					}
+					pointsPerScan[scan.Key] = points;
+				}
+
+				var s0 = pointsPerScan.First(x => x.Key.Contains('0'));
+
+				foreach (var s1 in pointsPerScan.Skip(1))
+				{
+					Point? mainScanP1 = null, newScanP1 = null, mainScanP2 = null, newScanP2 = null;
+					foreach (var p1 in s1.Value)
+					{
+						foreach (var p0 in s0.Value)
+						{
+							var ins = p0.DistancesToOtherPoints.Intersect(p1.DistancesToOtherPoints, new DistanceEqualityComparer()).ToList();
+							if (ins.Count() >= 11)
+							{
+								if (mainScanP1 == null)
+								{
+									mainScanP1 = p0;
+									newScanP1 = p1;
+								}
+								else if (mainScanP2 == null)
+								{
+									mainScanP2 = p0;
+									newScanP2 = p1;
+								}
+							}
+						}
+					}
+
+					int x1Dif, y1Dif, z1Dif, x2Dif, y2Dif, z2Dif;
+					if (mainScanP1 != null)
+					{
+						x1Dif = mainScanP1.Coordinates.X - mainScanP2.Coordinates.X;
+						x2Dif = newScanP1.Coordinates.X - newScanP2.Coordinates.X;
+
+						y1Dif = mainScanP1.Coordinates.Y - mainScanP2.Coordinates.Y;
+						y2Dif = newScanP1.Coordinates.Y - newScanP2.Coordinates.Y;
+
+						z1Dif = mainScanP1.Coordinates.Z - mainScanP2.Coordinates.Z;
+						z2Dif = newScanP1.Coordinates.Z - newScanP2.Coordinates.Z;
+
+						var map = new Dictionary<string, string>();
+						string xMap = null, yMap = null, zMap = null;
+						#region Create map
+						if (Math.Abs(x1Dif) == Math.Abs(x2Dif))
+						{
+							if (x1Dif == x2Dif)
+							{
+								xMap = "x";
+							}
+							else
+							{
+								xMap = "-x";
+							}
+						}
+						else if (Math.Abs(x1Dif) == Math.Abs(y2Dif))
+						{
+							if (x1Dif == y2Dif)
+							{
+								xMap = "y";
+							}
+							else
+							{
+								xMap = "-y";
+							}
+						}
+						else if (Math.Abs(x1Dif) == Math.Abs(z2Dif))
+						{
+							if (x1Dif == z2Dif)
+							{
+								xMap = "z";
+							}
+							else
+							{
+								xMap = "-z";
+							}
+						}
+
+						if (Math.Abs(y1Dif) == Math.Abs(x2Dif))
+						{
+							if (y1Dif == x2Dif)
+							{
+								yMap = "x";
+							}
+							else
+							{
+								yMap = "-x";
+							}
+						}
+						else if (Math.Abs(y1Dif) == Math.Abs(y2Dif))
+						{
+							if (y1Dif == y2Dif)
+							{
+								yMap = "y";
+							}
+							else
+							{
+								yMap = "-y";
+							}
+						}
+						else if (Math.Abs(y1Dif) == Math.Abs(z2Dif))
+						{
+							if (y1Dif == z2Dif)
+							{
+								yMap = "z";
+							}
+							else
+							{
+								yMap = "-z";
+							}
+						}
+
+						if (Math.Abs(z1Dif) == Math.Abs(x2Dif))
+						{
+							if (z1Dif == x2Dif)
+							{
+								zMap = "x";
+							}
+							else
+							{
+								zMap = "-x";
+							}
+						}
+						else if (Math.Abs(z1Dif) == Math.Abs(y2Dif))
+						{
+							if (z1Dif == y2Dif)
+							{
+								zMap = "y";
+							}
+							else
+							{
+								zMap = "-y";
+							}
+						}
+						else if (Math.Abs(z1Dif) == Math.Abs(z2Dif))
+						{
+							if (z1Dif == z2Dif)
+							{
+								zMap = "z";
+							}
+							else
+							{
+								zMap = "-z";
+							}
+						}
+						#endregion
+
+						var newPointCoords = new Coordinates(newScanP1.Coordinates.GetByAxis(xMap), newScanP1.Coordinates.GetByAxis(yMap), newScanP1.Coordinates.GetByAxis(zMap));
+						var mainPointCoords = mainScanP1.Coordinates;
+						var xDiff = newPointCoords.X - mainPointCoords.X;
+						var yDiff = newPointCoords.Y - mainPointCoords.Y;
+						var zDiff = newPointCoords.Z - mainPointCoords.Z;
+
+						scanners.Add(new(xDiff, yDiff, zDiff));
+						var mainScanPoints = s0.Value.Select(x => x.Coordinates);
+						var pointsToAdd = new List<Coordinates>();
+
+						foreach (var point in s1.Value.Select(x => x.Coordinates))
+						{
+							var z = new Coordinates(point.GetByAxis(xMap) - xDiff, point.GetByAxis(yMap) - yDiff, point.GetByAxis(zMap) - zDiff);
+							pointsToAdd.Add(z);
+						}
+
+						var all = mainScanPoints.Union(pointsToAdd);
+						coordinatesPerScan[s0.Key] = all.ToList();
+						coordinatesPerScan.Remove(s1.Key);
+						break;
+					}
+				}
+			}
+
+			var manhattan = new List<int>();
+			foreach (var scanner in scanners)
+			{
+				foreach (var scanner2 in scanners)
+				{
+					var distance = Math.Abs(scanner.X - scanner2.X) + Math.Abs(scanner.Y - scanner2.Y) + Math.Abs(scanner.Z - scanner2.Z);
+					manhattan.Add(distance);
+				}
+			}
+
+			return manhattan.Max();
 		}
 
 		public record Point(Coordinates Coordinates, List<Distance> DistancesToOtherPoints);
