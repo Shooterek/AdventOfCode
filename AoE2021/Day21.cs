762e098e7053d9f9ce2378e8859e1d9db7b11ab5
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2021-12-21 21:08:58 +0100

    Day 21 t1

--- AoE2021/Day21.cs
+++ AoE2021/Day21.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Text.RegularExpressions;
 using AoE2021.Utils;
 
@@ -12,13 +9,38 @@ namespace AoE2021
 		public Day21() : base("day21")
 		{
 		}
+
 		protected override object FirstTask()
 		{
-			var input = this._inputLoader.LoadEntireString();
+			var input = this._inputLoader.LoadStringListInput();
+			var p1StartPos = int.Parse(Regex.Matches(input[0], @"\d+")[1].Value);
+			var p2StartPos = int.Parse(Regex.Matches(input[1], @"\d+")[1].Value);
 
-			var matches = Regex.Matches(input, @"\d+");
-			Console.WriteLine(string.Join(", ", matches.Select(x => x.Value)));
-			return "";
+			var p1Score = 0;
+			var p2Score = 0;
+			var dieCounter = 0;
+			var currVal = 6;
+			while (p1Score < 1000 && p2Score < 1000)
+			{
+				p1StartPos += currVal--;
+				p1StartPos = p1StartPos > 10 ? p1StartPos % 10 : p1StartPos;
+				p1Score += p1StartPos;
+				dieCounter += 3;
+				currVal = currVal < 0 ? 9 : currVal;
+
+				if (p1Score >= 1000)
+					break;
+
+				p2StartPos += currVal--;
+				p2StartPos = p2StartPos > 10 ? p2StartPos % 10 : p2StartPos;
+				p2Score += p2StartPos;
+				currVal = currVal < 0 ? 9 : currVal;
+				dieCounter += 3;
+
+				Console.WriteLine($"P1: {p1Score}, P2: {p2Score}");
+			}
+
+			return Math.Min(p1Score, p2Score) * dieCounter;
 		}
 
 		protected override object SecondTask()
