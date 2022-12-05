f2862f013d5d00747599daf7ed80bf90ac9ce847
Bartłomiej Płókarz <bartlomiej.plokarz@speeron.com>
2022-12-05 11:38:43 +0100

    Day5 start

---
+++ AoE2022/Days/Day5.cs
@@ -0,0 +1,24 @@
+using AoE2022.Utils;
+
+public class Day5 : StringBatchesDay
+{
+    protected override object FirstTask()
+    {
+        var startingPos = this.Input.First().Split("\r ");
+        var commands = this.Input.Skip(1).First().Split("\r ");
+
+        var queues = new List<Queue<char>>(10);
+
+        var index = 1;
+        while (true) {
+            foreach (var line in )
+        }
+
+        return null;
+    }
+
+    protected override object SecondTask()
+    {
+        return null;
+    }
+}
