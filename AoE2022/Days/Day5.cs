c4553c9bcb1665a7b17d4f8e4879e1ba81a2f419
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2022-12-07 20:58:29 +0100

    Day 5 refactor

--- AoE2022/Days/Day5.cs
+++ AoE2022/Days/Day5.cs
@@ -4,32 +4,10 @@ public class Day5 : StringBatchesDay
 {
     protected override object FirstTask()
     {
-        var startingPos = this.Input.First().Split("\r ");
-        var commands = this.Input.Skip(1).First().Split("\r ");
-
-        var stacks = new Stack<char>[10];
-
-        var index = 1;
-        while (index < startingPos.MaxBy(c => c.Length).Length) {
-            var stackIndex = (int)Char.GetNumericValue(startingPos.Last()[index]) - 1;
-            stacks[stackIndex] = new Stack<char>();
-            for (int i = startingPos.Length - 2; i >= 0; i--) {
-                var letter = startingPos[i][index];
-                if (letter == ' ')
-                    continue;
-
-                stacks[stackIndex].Push(letter);
-            }
-            index += 4;
-        }
-
-        foreach (var command in commands) {
-            var parts = command.Split(' ');
-            var numbers = new string[] { parts[1], parts[3], parts[5] }.Select(int.Parse).ToArray();
-            var count = numbers[0];
-            var from = numbers[1] - 1;
-            var to = numbers[2] - 1;
-
+        var stacks = CreateInitialStacks();
+        var commands = this.Input.Skip(1).First().Split("\r ").Select(ParseCommand);
+        foreach ((var count, var from, var to) in commands)
+        {
             for (int i = 0; i < count; i++)
             {
                 stacks[to].Push(stacks[from].Pop());
@@ -40,16 +18,39 @@ public class Day5 : StringBatchesDay
     }
 
     protected override object SecondTask()
-    {        var startingPos = this.Input.First().Split("\r ");
-        var commands = this.Input.Skip(1).First().Split("\r ");
+    {
+        var stacks = CreateInitialStacks();
+        var commands = this.Input.Skip(1).First().Split("\r ").Select(ParseCommand);
+        foreach ((var count, var from, var to) in commands)
+        {
+            string move = "";
+            for (int i = 0; i < count; i++)
+            {
+                move += stacks[from].Pop();
+            }
 
+            foreach (var letter in move.Reverse())
+            {
+                stacks[to].Push(letter);
+            }
+        }
+
+        return string.Join("", stacks.OfType<Stack<char>>().Select(q => q.Pop()));
+    }
+
+    private Stack<char>[] CreateInitialStacks()
+    {
         var stacks = new Stack<char>[10];
 
         var index = 1;
-        while (index < startingPos.MaxBy(c => c.Length).Length) {
+        var startingPos = this.Input.First().Split("\r ");
+        var maxLength = startingPos.Max(c => c.Length);
+        while (index < maxLength)
+        {
             var stackIndex = (int)Char.GetNumericValue(startingPos.Last()[index]) - 1;
             stacks[stackIndex] = new Stack<char>();
-            for (int i = startingPos.Length - 2; i >= 0; i--) {
+            for (int i = startingPos.Length - 2; i >= 0; i--)
+            {
                 var letter = startingPos[i][index];
                 if (letter == ' ')
                     continue;
@@ -59,24 +60,17 @@ public class Day5 : StringBatchesDay
             index += 4;
         }
 
-        foreach (var command in commands) {
-            var parts = command.Split(' ');
-            var numbers = new string[] { parts[1], parts[3], parts[5] }.Select(int.Parse).ToArray();
-            var count = numbers[0];
-            var from = numbers[1] - 1;
-            var to = numbers[2] - 1;
+        return stacks;
+    }
 
-            string move = "";
-            for (int i = 0; i < count; i++)
-            {
-                move += stacks[from].Pop();
-            }
+    private (int, int, int) ParseCommand(string command)
+    {
+        var parts = command.Split(' ');
+        var numbers = new string[] { parts[1], parts[3], parts[5] }.Select(int.Parse).ToArray();
+        var count = numbers[0];
+        var from = numbers[1] - 1;
+        var to = numbers[2] - 1;
 
-            foreach (var letter in move.Reverse()) {
-                stacks[to].Push(letter);
-            }
-        }
-
-        return string.Join("", stacks.OfType<Stack<char>>().Select(q => q.Pop()));
+        return (count, from, to);
     }
 }
