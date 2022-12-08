fd12cb487447f27ec48e0605142007bcaf8bbc41
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2022-12-08 20:40:36 +0100

    Day 8

--- AoE2022/Days/Day8.cs
+++ AoE2022/Days/Day8.cs
@@ -1,47 +1,56 @@
 using AoE2022.Utils;
+using MoreLinq;
 
 public class Day8 : StringListDay
 {
     protected override object FirstTask()
     {
         var numbers = this.Input.Select(l => l.Select(n => (int)Char.GetNumericValue(n)).ToArray()).ToArray();
-        var right = numbers.Select((array, index) => Count(array.WithIndex(), null, index)).SelectMany(l => l);
-        var left = numbers.Select((array, index) => Count(array.WithIndex().Reverse(), null, index)).SelectMany(l => l);
-        var down = UpToDown(numbers).Select((array, index) => Count(array.WithIndex(), index, null)).SelectMany(l => l);
-        var up = UpToDown(numbers).Select((array, index) => Count(array.WithIndex().Reverse(), index, null)).SelectMany(l => l);
-        var count = right
-            .Concat(left)
-            .Concat(up) 
-            .Concat(down)
-            .Distinct();
+        var upToDown = UpToDown(numbers).ToList();
+        var counter = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = 0; j < numbers[i].Length; j++)
+            {
+                var currentTree = numbers[i][j];
+                if (numbers[i][0..j].All(v => v < currentTree)
+                    || numbers[i][(j + 1)..].All(v => v < currentTree)
+                    || upToDown[j][0..i].All(v => v < currentTree)
+                    || upToDown[j][(i + 1)..].All(v => v < currentTree))
+                    {
+                        counter++;
+                    }
+            }
+        }
 
-        var x = count.Where(p => numbers[p.X][p.Y] == 0).ToList();
-        return count.Count();
+        return counter;
     }
 
     protected override object SecondTask()
     {
-        throw new NotImplementedException();
+        var numbers = this.Input.Select(l => l.Select(n => (int)Char.GetNumericValue(n)).ToArray()).ToArray();
+        var upToDown = UpToDown(numbers).ToList();
+        var max = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = 0; j < numbers[i].Length; j++)
+            {
+                var currentTree = numbers[i][j];
+                var left = numbers[i][0..j].Reverse().TakeUntil(v => v >= currentTree).Count();
+                var right = numbers[i][(j + 1)..].TakeUntil(v => v >= currentTree).Count();
+                var up = upToDown[j][0..i].Reverse().TakeUntil(v => v >= currentTree).Count();
+                var down = upToDown[j][(i + 1)..].TakeUntil(v => v >= currentTree).Count();
+
+                var score = left * right * up * down;
+                if (score > max)
+                    max = score;
+            }
+        }
+
+        return max;
     }
 
-    private List<Point> Count(IEnumerable<(int item, int index)> source, int? x, int? y) => 
-        source.Aggregate((visible: true, list: new List<Point>(), max: -1), (state, next) =>
-        {
-            if (state.visible && next.item > state.max )
-            {
-                state.list.Add(new(x ?? next.index, y ?? next.index));
-                state.max = next.item;
-                return state;
-            }
-            else if (next.item == state.max) {
-                return state;
-            }
-
-            state.visible = false;
-            return state;
-        }).list;
-
-    private IEnumerable<IEnumerable<int>> UpToDown(int[][] source) {
+    private IEnumerable<int[]> UpToDown(int[][] source) {
         for (int i = 0; i < source[0].Length; i++)
         {
             var r = new List<int>();
@@ -50,15 +59,7 @@ public class Day8 : StringListDay
                 r.Add(source[j][i]);
             }
 
-            yield return r;
+            yield return r.ToArray();
         }
     }
-
-    private record Point(int X, int Y);
-}
-
-public static class IEnumerableExtensions
-{
-    public static IEnumerable<(T, int)> WithIndex<T> (this IEnumerable<T> source)
-        => source.Select((item, index) => (item, index));
 }
