b9668fbc956fd79b53d16317b81c7073d31f058b
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2022-12-11 18:16:42 +0100

    Day 11 P2 some hardcoded values

--- AoE2022/Days/Day11.cs
+++ AoE2022/Days/Day11.cs
@@ -6,23 +6,23 @@ public class Day11 : StringBatchesDay
     private static Regex NumberPattern = new Regex(@"\d+");
     protected override object FirstTask()
     {
-        var monkeys = new Dictionary<int, Monkey>();
+        var monkeys = new Dictionary<long, Monkey>();
 
         foreach (var batch in this.Input) {
             var lines = batch.Split("\r ");
-            var monkeyNumber = int.Parse(NumberPattern.Match(lines[0]).Captures[0].Value);
-            var items = NumberPattern.Matches(lines[1]).Select(c => int.Parse(c.Captures[0].Value));
-            var operationValue = NumberPattern.Match(lines[2]) is Match { Success: true } m ? int.Parse(NumberPattern.Match(lines[2]).Captures[0].Value) : null as int?;
+            var monkeyNumber = long.Parse(NumberPattern.Match(lines[0]).Captures[0].Value);
+            var items = NumberPattern.Matches(lines[1]).Select(c => long.Parse(c.Captures[0].Value));
+            var operationValue = NumberPattern.Match(lines[2]) is Match { Success: true } m ? long.Parse(NumberPattern.Match(lines[2]).Captures[0].Value) : null as long?;
             var operation = lines[2].Contains("*") ? Operation.Multiply : Operation.Add;
-            var divisibleBy = int.Parse(NumberPattern.Match(lines[3]).Captures[0].Value);
-            var trueMonkey = int.Parse(NumberPattern.Match(lines[4]).Captures[0].Value);
-            var falseMonkey = int.Parse(NumberPattern.Match(lines[5]).Captures[0].Value);
+            var divisibleBy = long.Parse(NumberPattern.Match(lines[3]).Captures[0].Value);
+            var trueMonkey = long.Parse(NumberPattern.Match(lines[4]).Captures[0].Value);
+            var falseMonkey = long.Parse(NumberPattern.Match(lines[5]).Captures[0].Value);
             monkeys[monkeyNumber] = new Monkey(items.ToList(), divisibleBy, trueMonkey, falseMonkey, operation, operationValue);
         }
 
-        for (int i = 0; i < 20; i++)
+        for (long i = 0; i < 20; i++)
         {
-            for (int j = 0; j < monkeys.Count(); j++)
+            for (long j = 0; j < monkeys.Count(); j++)
             {
                 foreach ((var target, var item) in monkeys[j].InspectItems()) {
                     monkeys[target].Items.Add(item);
@@ -32,43 +32,43 @@ public class Day11 : StringBatchesDay
             }
         }
 
-        return monkeys.Select(m => m.Value).OrderByDescending(m => m.InspectedItems).Take(2).Aggregate(1, (current, next) => current * next.InspectedItems);
+        return monkeys.Select(m => m.Value).OrderByDescending(m => m.InspectedItems).Take(2).Aggregate(1L, (current, next) => current * next.InspectedItems);
     }
 
     protected override object SecondTask()
     {
-        var monkeys = new Dictionary<int, Monkey>();
+        var monkeys = new Dictionary<long, Monkey>();
 
         foreach (var batch in this.Input) {
             var lines = batch.Split("\r ");
-            var monkeyNumber = int.Parse(NumberPattern.Match(lines[0]).Captures[0].Value);
-            var items = NumberPattern.Matches(lines[1]).Select(c => int.Parse(c.Captures[0].Value));
-            var operationValue = NumberPattern.Match(lines[2]) is Match { Success: true } m ? int.Parse(NumberPattern.Match(lines[2]).Captures[0].Value) : null as int?;
+            var monkeyNumber = long.Parse(NumberPattern.Match(lines[0]).Captures[0].Value);
+            var items = NumberPattern.Matches(lines[1]).Select(c => long.Parse(c.Captures[0].Value));
+            var operationValue = NumberPattern.Match(lines[2]) is Match { Success: true } m ? long.Parse(NumberPattern.Match(lines[2]).Captures[0].Value) : null as long?;
             var operation = lines[2].Contains("*") ? Operation.Multiply : Operation.Add;
-            var divisibleBy = int.Parse(NumberPattern.Match(lines[3]).Captures[0].Value);
-            var trueMonkey = int.Parse(NumberPattern.Match(lines[4]).Captures[0].Value);
-            var falseMonkey = int.Parse(NumberPattern.Match(lines[5]).Captures[0].Value);
+            var divisibleBy = long.Parse(NumberPattern.Match(lines[3]).Captures[0].Value);
+            var trueMonkey = long.Parse(NumberPattern.Match(lines[4]).Captures[0].Value);
+            var falseMonkey = long.Parse(NumberPattern.Match(lines[5]).Captures[0].Value);
             monkeys[monkeyNumber] = new Monkey(items.ToList(), divisibleBy, trueMonkey, falseMonkey, operation, operationValue, false);
         }
         
-        for (int i = 0; i < 200; i++)
+        for (long i = 0; i < 10000; i++)
         {
-            for (int j = 0; j < monkeys.Count(); j++)
+            for (long j = 0; j < monkeys.Count(); j++)
             {
                 var moves = monkeys[j].InspectItems();
                 foreach ((var target, var item) in moves) {
-                    monkeys[target].Items.Add(item);
+                    monkeys[target].Items.Add(item % 9699690);
                 }
                 monkeys[j] = monkeys[j] with { Items = new() };
             }
         }
 
-        return monkeys.Select(m => m.Value).OrderByDescending(m => m.InspectedItems).Take(2).Aggregate(1, (current, next) => current * next.InspectedItems);
+        return monkeys.Select(m => m.Value).OrderByDescending(m => m.InspectedItems).Take(2).Aggregate(1L, (current, next) => current * next.InspectedItems);
     }
 
-    private record Monkey(List<int> Items, int DivisibleBy, int MonkeyTrue, int MonkeyFalse, Operation Operation, int? OperationValue, bool Worry = true) {
-        public int InspectedItems { get; private set; }
-        public List<(int target, int item)> InspectItems()
+    private record Monkey(List<long> Items, long DivisibleBy, long MonkeyTrue, long MonkeyFalse, Operation Operation, long? OperationValue, bool Worry = true) {
+        public long InspectedItems { get; private set; }
+        public List<(long target, long item)> InspectItems()
         {
             this.InspectedItems += this.Items.Count;
             return this.Items.Select(i =>
