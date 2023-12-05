ea883a551cd89dba37f15aaaf78236c7eb926e7c
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2023-12-05 14:17:51 +0100

    Refactor

--- AoE2023/Days/Day5.cs
+++ AoE2023/Days/Day5.cs
@@ -6,7 +6,11 @@ public class Day5 : StringBatchesDay
 {
     protected override object FirstTask()
     {
-        var seeds = this.Input.First().Split(":")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+        var seeds = this.Input.First()
+            .Split(":")[1]
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToArray();
         var transformers = this.Input.Skip(1).Select(batch => {
             return batch.Split("\r").Skip(1).Select(line => {
                 var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
@@ -32,8 +36,25 @@ public class Day5 : StringBatchesDay
 
     protected override object SecondTask()
     {
+        var seeds = this.Input.First()
+            .Split(":")[1]
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .Chunk(2)
+            .Select(chunk => new CRange(chunk[0], chunk[1]))
+            .ToArray();
+
+        var transformers = this.Input.Skip(1).Select(batch => {
+            return batch.Split("\r").Skip(1).Select(line => {
+                var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                return new Transformer(numbers[0], numbers[1], numbers[2]);
+            }).ToArray();
+        }).ToArray();
+
         return null;
     }
 }
 
 file record Transformer(long Destination, long Source, long Range);
+
+file record CRange(long Start, long Count);
