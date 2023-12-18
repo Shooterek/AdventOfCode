b2e073b54ab5b84fa8a4d77a7341c6c1d117de18
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2023-12-18 18:07:17 +0100

    P2 progress

--- AoE2023/Days/Day18.cs
+++ AoE2023/Days/Day18.cs
@@ -1,6 +1,4 @@
 using AoE2023.Utils;
-using Microsoft.Diagnostics.Runtime.Utilities;
-using System.Text.RegularExpressions;
 
 namespace AoE2023;
 
@@ -11,7 +9,7 @@ public class Day18 : StringListDay
         var instructions = this.Input.Select(line =>
         {
             var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-            return new PaintInstruction(parts[0].First(), int.Parse(parts[1]), parts[2]);
+            return new PaintInstruction(parts[0].First(), int.Parse(parts[1]));
         }).ToArray();
 
         var points = new HashSet<Coordinates>();
@@ -37,7 +35,7 @@ public class Day18 : StringListDay
         var xOffset = points.MinBy(sq => sq.X).X;
         var yOffset = points.MinBy(sq => sq.Y).Y;
 
-        points = points.Select(p => p with { X = p.X - xOffset, Y = p.Y -yOffset }).ToHashSet();
+        points = points.Select(p => p with { X = p.X - xOffset, Y = p.Y - yOffset }).ToHashSet();
 
         var maxX = points.MaxBy(sq => sq.X).X + 1;
         var maxY = points.MaxBy(sq => sq.Y).Y + 1;
@@ -74,23 +72,90 @@ public class Day18 : StringListDay
 
     protected override object SecondTask()
     {
-        return null;
-    }
-
-
-    static List<Coordinates> GenerateCoordinates(int rows, int columns)
-    {
-        var coordinates = new List<Coordinates>();
-
-        for (int x = 0; x < columns; x++)
+        var instructions = this.Input.Select(line =>
         {
-            for (int y = 0; y < rows; y++)
+            var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var hex = int.Parse(parts[2][2..7], System.Globalization.NumberStyles.HexNumber);
+            return new PaintInstruction(parts[2][7], hex);
+        }).ToArray();
+
+        var points = new HashSet<Coordinates>();
+        var start = new Coordinates(0, 0);
+        points.Add(start);
+        foreach (var instr in instructions)
+        {
+            var change = instr.Direction switch
             {
-                coordinates.Add(new(x, y));
+                '0' => (1, 0),
+                '2' => (-1, 0),
+                '3' => (0, -1),
+                '1' => (0, 1),
+            };
+
+            start = start with { X = start.X + change.Item1 * instr.Length, Y = start.Y + change.Item2 * instr.Length };
+            points.Add(start);
+        }
+
+        var pointsPrim = new HashSet<Coordinates>();
+        start = new Coordinates(0, 0);
+        pointsPrim.Add(start);
+        foreach (var instr in instructions)
+        {
+            var change = instr.Direction switch
+            {
+                '0' => (1, 0),
+                '2' => (-1, 0),
+                '3' => (0, -1),
+                '1' => (0, 1),
+            };
+
+            start = start with { X = start.X + change.Item1, Y = start.Y + change.Item2 };
+            pointsPrim.Add(start);
+        }
+
+        var xOffset = pointsPrim.MinBy(sq => sq.X).X;
+        var yOffset = pointsPrim.MinBy(sq => sq.Y).Y;
+
+        pointsPrim = pointsPrim.Select(p => p with { X = p.X - xOffset, Y = p.Y - yOffset }).ToHashSet();
+
+        var maxX = pointsPrim.MaxBy(sq => sq.X).X + 1;
+        var maxY = pointsPrim.MaxBy(sq => sq.Y).Y + 1;
+
+        var wallSquares = new HashSet<Coordinates>();
+        foreach (var x in Enumerable.Range(0, maxX))
+        {
+            wallSquares.Add(new(x, 0));
+            wallSquares.Add(new(x, maxY - 1));
+        }
+        foreach (var y in Enumerable.Range(0, maxY))
+        {
+            wallSquares.Add(new(0, y));
+            wallSquares.Add(new(maxX - 1, y));
+        }
+
+        wallSquares = wallSquares.Except(pointsPrim).ToHashSet();
+        var nextSquares = wallSquares.ToHashSet();
+        while (nextSquares.Any())
+        {
+            var next = nextSquares.SelectMany(sq => GetNeighbours(sq, maxX, maxY)).Distinct().ToArray();
+            nextSquares.Clear();
+            foreach (var sq in next)
+            {
+                if (!pointsPrim.Contains(sq) && wallSquares.Add(sq))
+                {
+                    nextSquares.Add(sq);
+                }
             }
         }
 
-        return coordinates;
+        var xOrder = points.Select(p => p.X).Order().ToArray();
+        var yOrder = points.Select(p => p.Y).Order().ToArray();
+        long allSquares = (points.MaxBy(p => p.X).X + 1) * (points.MaxBy(p => p.Y).Y + 1);
+        foreach (var wall in wallSquares) {
+            allSquares -= ((long)Math.Abs(xOrder[wall.X + 1] - xOrder[wall.X])) * ((long)Math.Abs(yOrder[wall.Y + 1] - yOrder[wall.Y]));
+        }
+
+        return allSquares;
     }
 
     private IEnumerable<Coordinates> GetNeighbours(Coordinates current, int maxX, int maxY)
@@ -101,4 +166,4 @@ public class Day18 : StringListDay
     }
 }
 
-public record PaintInstruction(char Direction, int Length, string Color);
+public record PaintInstruction(char Direction, int Length);
