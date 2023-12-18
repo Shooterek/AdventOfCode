340388e58938dd96c9cd8ba89ecc4278f814276b
Bartłomiej Płókarz <plokarzbartlomiej@gmail.com>
2023-12-18 22:49:41 +0100

    Seemingly some progress towards part 2

--- AoE2023/Days/Day18.cs
+++ AoE2023/Days/Day18.cs
@@ -12,8 +12,8 @@ public class Day18 : StringListDay
             return new PaintInstruction(parts[0].First(), int.Parse(parts[1]));
         }).ToArray();
 
-        var points = new HashSet<Coordinates>();
-        var start = new Coordinates(0, 0);
+        var points = new HashSet<Coordinates2>();
+        var start = new Coordinates2(0, 0);
         points.Add(start);
         foreach (var instr in instructions)
         {
@@ -40,13 +40,13 @@ public class Day18 : StringListDay
         var maxX = points.MaxBy(sq => sq.X).X + 1;
         var maxY = points.MaxBy(sq => sq.Y).Y + 1;
 
-        var wallSquares = new HashSet<Coordinates>();
-        foreach (var x in Enumerable.Range(0, maxX))
+        var wallSquares = new HashSet<Coordinates2>();
+        foreach (var x in Enumerable.Range(0, (int)maxX))
         {
             wallSquares.Add(new(x, 0));
             wallSquares.Add(new(x, maxY - 1));
         }
-        foreach (var y in Enumerable.Range(0, maxY))
+        foreach (var y in Enumerable.Range(0, (int)maxY))
         {
             wallSquares.Add(new(0, y));
             wallSquares.Add(new(maxX - 1, y));
@@ -75,20 +75,20 @@ public class Day18 : StringListDay
         var instructions = this.Input.Select(line =>
         {
             var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-            return new PaintInstruction(parts[0].First(), int.Parse(parts[1]));
+            return new PaintInstruction(parts[2][7], long.Parse(parts[2][2..7], System.Globalization.NumberStyles.HexNumber));
         }).ToArray();
 
-        var points = new HashSet<Coordinates>();
-        var start = new Coordinates(0, 0);
+        var points = new HashSet<Coordinates2>();
+        var start = new Coordinates2(0, 0);
         points.Add(start);
         foreach (var instr in instructions)
         {
             var change = instr.Direction switch
             {
-                'R' => (1, 0),
-                'L' => (-1, 0),
-                'U' => (0, -1),
-                'D' => (0, 1),
+                '0' => (1, 0),
+                '2' => (-1, 0),
+                '3' => (0, -1),
+                '1' => (0, 1),
             };
 
             start = start with { X = start.X + change.Item1 * instr.Length, Y = start.Y + change.Item2 * instr.Length };
@@ -99,10 +99,10 @@ public class Day18 : StringListDay
         var xOrder = points.Select(p => p.X).Order().Distinct().ToList();
         var yOrder = points.Select(p => p.Y).Order().Distinct().ToList();
 
-        IEnumerable<int> n = xOrder.SelectMany(p => new int[] { p - 1, p + 1 }).Where(p => p > xOrder[0] && p < xOrder.Last()).ToArray();
+        IEnumerable<long> n = xOrder.SelectMany(p => new long[] { p - 1, p + 1 }).Where(p => p > xOrder[0] && p < xOrder.Last()).ToArray();
         xOrder.AddRange(n);
 
-        n = yOrder.SelectMany(p => new int[] { p - 1, p + 1 }).Where(p => p > yOrder[0] && p < yOrder.Last()).ToArray();
+        n = yOrder.SelectMany(p => new long[] { p - 1, p + 1 }).Where(p => p > yOrder[0] && p < yOrder.Last()).ToArray();
         yOrder.AddRange(n);
 
         xOrder = [.. xOrder.Distinct().Order()];
@@ -112,15 +112,15 @@ public class Day18 : StringListDay
         {
             var change = instr.Direction switch
             {
-                'R' => (1, 0),
-                'L' => (-1, 0),
-                'U' => (0, -1),
-                'D' => (0, 1),
+                '0' => (1, 0),
+                '2' => (-1, 0),
+                '3' => (0, -1),
+                '1' => (0, 1),
             };
         }
 
         var pointList = points.ToArray();
-        var pointsPrim = new HashSet<Coordinates>();
+        var pointsPrim = new HashSet<Coordinates2>();
         for (int i = 0; i < points.Count; i++)
         {
             var currentX = xOrder.FindIndex(c => pointList[i].X == c);
@@ -147,7 +147,7 @@ public class Day18 : StringListDay
         var maxX = xOrder.Count + 1;
         var maxY = yOrder.Count + 1;
 
-        var wallSquares = new HashSet<Coordinates>();
+        var wallSquares = new HashSet<Coordinates2>();
         foreach (var x in Enumerable.Range(0, maxX))
         {
             wallSquares.Add(new(x, 0));
@@ -176,31 +176,34 @@ public class Day18 : StringListDay
 
         long allSquares = (points.MaxBy(p => p.X).X + 1) * (points.MaxBy(p => p.Y).Y + 1);
 
-
+        var temp = new List<long>();
         DisplayJaggedArray(xOrder.Count, yOrder.Count, wallSquares.ToHashSet());
 
         for (int x = 0; x < xOrder.Count; x++) {
             for (int y = 0; y < yOrder.Count; y++) {
                 if (wallSquares.Contains(new(x, y))) {
-                    var xCount = xOrder[x] - x > 0 ? xOrder[x - 1] : 1;
-                    var yCount = yOrder[y] - y > 0 ? yOrder[y - 1] : 1;
-
-                    allSquares -= xCount * yCount;
+                    var xCount = (xOrder[x]) - (x > 0 ? xOrder[x - 1] + 1 : 0);
+                    var yCount = (yOrder[y]) - (y > 0 ? yOrder[y - 1] + 1 : 0);
+                    temp.Add(yCount);
+                    temp.Add(xCount);
+                    allSquares -= xCount * yCount + 1;
                 }
             }
         }
 
-        return allSquares;
+        var t = temp.Sum();
+        Console.WriteLine(t);
+        return allSquares- t;
     }
 
-    private IEnumerable<Coordinates> GetNeighbours(Coordinates current, int maxX, int maxY)
+    private IEnumerable<Coordinates2> GetNeighbours(Coordinates2 current, long maxX, long maxY)
     {
-        return new Coordinates[] { new(0, -1), new(0, 1), new(1, 0), new(-1, 0) }
-            .Select(pd => new Coordinates(current.X + pd.X, current.Y + pd.Y))
+        return new Coordinates2[] { new(0, -1), new(0, 1), new(1, 0), new(-1, 0) }
+            .Select(pd => new Coordinates2(current.X + pd.X, current.Y + pd.Y))
             .Where(c => c.X >= 0 && c.X < maxX && c.Y >= 0 && c.Y < maxY);
     }
 
-    static void DisplayJaggedArray(int maxX, int maxY, HashSet<Coordinates> set)
+    static void DisplayJaggedArray(int maxX, int maxY, HashSet<Coordinates2> set)
     {
         for (int i = 0; i < maxY; i++)
         {
@@ -214,4 +217,6 @@ public class Day18 : StringListDay
     }
 }
 
-public record PaintInstruction(char Direction, int Length);
+public record PaintInstruction(char Direction, long Length);
+
+public record Coordinates2(long X, long Y);
